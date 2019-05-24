e6247908fbb1af3052cbada2b55de8f5e1a1218c
ZCreturn0 <237350543@qq.com>
2019-05-24 23:27:02 +0800

    成功失败回调

--- promise/readFile.js
+++ promise/readFile.js
@@ -1,14 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
-function getFileData(file, callback){
+function getFileData(file, successCallback, failCallback){
     fs.readFile(file, 'utf-8', (err, data) => {
         if(err){
-            throw err;
+            return failCallback(err, data);
         }
-        callback(data);
+        else{
+            return successCallback(err, data);
+        }
     });
 }
-getFileData(path.resolve(__dirname, './files/2.txt'), (data) => {
+getFileData(path.resolve(__dirname, './files/1.txt'), (err, data) => {
+    console.log('success');
+    console.log(data);
+}, (err, data) => {
+    console.log('fail');
     console.log(data);
 });
