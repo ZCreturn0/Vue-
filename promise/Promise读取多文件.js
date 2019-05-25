6acd69317cf00458d01369f2edb97dd9fb158ad1
ZCreturn0 <237350543@qq.com>
2019-05-25 23:51:20 +0800

    Promise读取多文件

---
+++ promise/Promise读取多文件.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+function getFileByPath(filename) {
+    var promise = new Promise(function (resolve, reject) {
+        fs.readFile(filename, 'utf-8', function (err, data) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        });
+    });
+    return promise;
+}
+
+getFileByPath('./files/1.txt').then((data) => {
+    console.log('success');
+    console.log(data);
+    return getFileByPath('./files/2.txt');
+}, (err) => {
+    console.log('fail');
+    console.log(err);
+}).then((data) => {
+    console.log('success');
+    console.log(data);
+});
